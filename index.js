22a33c5b25678ce0213345944ed97a0fdfd765a5
jvitorc <jvitoroc@gmail.com>
2018-07-03 19:21:20 -0300

    new transition model

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-let element = document.getElementById('data-simple-slide')
+let element = document.getElementById('data-simple-slide');
 
-function SlideShow(element, images, delay){
-    this.element = element;
+let unactiveStyle = {
+    zIndex: 0,
+    opacity: 0,
+}
+
+let activeStyle = {
+    zIndex: 1,
+    opacity: 1,
+}
+
+function SlideShow(element, images, transition, delay){
+    this.transition = transition;
     this.delay = delay;
-    this.playing = false;
-    this.numberOfSlides = this._populate(images);
-    this.currentState = [0, 1];
 
+    this.element = element;
+    this.playing = false;
+    this.numberOfSlides = this._populate(images, {transition:`opacity ${transition}ms ease-in-out`});
+    this.currentState = [-1, 0, 1];
+    this._continue();
     this.slideChangeCallbacks = [];
 }
 
-SlideShow.prototype._populate = function(images){
+SlideShow.prototype._populate = function(images, initialStyle){
     let slides;
 
     if(images === true)
@@ -30,28 +42,32 @@ SlideShow.prototype._populate = function(images){
         return buffer;
     });
 
+    let initialActiveStyle = Object.assign({}, activeStyle, initialStyle);
+    let initialUnactiveStyle = Object.assign({}, unactiveStyle, initialStyle);
+
     for(let i = 0; i < this.slides.length; i++){
-        if(i == 0)
-            this.slides[0].classList.add('active');
+        this._mergeSlideStyle(i, i === 0 ? initialActiveStyle:initialUnactiveStyle);
         if(images !== false)
             this.element.appendChild(this.slides[i]);
     }
-    
-    this.element.classList.add("transition");
+
     return this.slides.length;
 }
 
 SlideShow.prototype.next = function(){
     if(this.numberOfSlides === 0)
         return;
-    this.slides[this.currentState[0]].classList.remove('active');
-    this.slides[this.currentState[1]].classList.add('active');
-    this._continue();
+    this._mergeSlideStyle(this.currentState[0], unactiveStyle);
+    this._mergeSlideStyle(this.currentState[1], activeStyle);
+}
+
+SlideShow.prototype._mergeSlideStyle = function(slideIndex, style){
+    Object.assign(this.slides[slideIndex].style, style);
 }
 
 SlideShow.prototype._continue = function(){
     for(let i = 0; i < this.numberOfSlides; i++){
-        if(this.currentState[i] === this.numberOfSlides-1)
+        if(this.currentState[i] >= this.numberOfSlides-1)
             this.currentState[i] = 0;
         else
             this.currentState[i] += 1;
@@ -59,16 +75,22 @@ SlideShow.prototype._continue = function(){
 }
 
 SlideShow.prototype.play = function(){
+    if(this.isPlaying())
+        return;
     this.playing = setInterval(()=>{
         this.next();
+        this._continue();
         this.slideChangeCallbacks.forEach((cb)=>{
             cb(this.currentState[0]);
         });
-    }, this.delay);
+    }, this.delay + this.transition/2);
 }
 
-SlideShow.prototype.setDelay = function(delay, immediately = true){
-    this.delay = delay;
+SlideShow.prototype.setTransitionDelay = function(transition, immediately = true){
+    this.transition = transition;
+    for(let i = 0; i < this.numberOfSlides; i++){
+        this._mergeSlideStyle(i, {transition: `opacity ${transition}ms ease-in-out`});
+    }
     if(immediately){
         this.refresh();
     }
@@ -82,8 +104,9 @@ SlideShow.prototype.refresh = function(){
 }
 
 SlideShow.prototype.stop = function(){
-    if(this.playing === false)
+    if(!this.isPlaying())
         return;
+    this.refresh();
     clearInterval(this.playing);
     this.playing = false;
 }
@@ -96,9 +119,15 @@ SlideShow.prototype.isPlaying = function(){
     return this.playing !== false;
 }
 
-let slideShow = new SlideShow(element, true, 1000);
+let slideShow = new SlideShow(element, true, 1000, 1000);
 slideShow.play();
 
 slideShow.onSlideChange((currentSlide)=>{
     console.log(currentSlide);
 });
+
+var slider = document.getElementById("myRange");
+
+slider.onchange = function() {
+    slideShow.setTransitionDelay(this.value);
+}
